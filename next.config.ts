a55684a5e05fe7ead8f99b2c1fe9019460529d17
kunwardeep <kunwardeep@gmail.com>
2025-04-06 00:09:01 +1100

    switching back to 2017

--- next.config.ts
+++ next.config.ts
@@ -6,10 +6,6 @@ const nextConfig: NextConfig = {
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
   },
-  webpack(config) {
-    config.resolve.alias["@"] = path.resolve(__dirname, "src");
-    return config;
-  },
 };
 
 export default nextConfig;
