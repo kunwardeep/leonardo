f6714cc3e40e91c042422ac02985d6d4dd2c9ae3
kunwardeep <kunwardeep@gmail.com>
2025-04-06 00:14:40 +1100

    Revert "switching back to 2017"
    This reverts commit a55684a5e05fe7ead8f99b2c1fe9019460529d17.

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,10 @@ const nextConfig: NextConfig = {
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
   },
+  webpack(config) {
+    config.resolve.alias["@"] = path.resolve(__dirname, "src");
+    return config;
+  },
 };
 
 export default nextConfig;
