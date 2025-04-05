1b65d43fb7a57d4e1611ff51c5e9e8f369be9800
kunwardeep <kunwardeep@gmail.com>
2025-04-06 00:06:41 +1100

    adding the alias config to webpack

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
+  },
+  webpack(config) {
+    config.resolve.alias["@"] = path.resolve(__dirname, "src");
+    return config;
   },
 };
 
