9dceea5267f1ffd9a95f04f8d159e6bb46806fb3
kunwardeep <kunwardeep@gmail.com>
2025-04-08 16:44:14 +1000

    add a blur image

--- src/components/DisplayCharacters/CharacterAvatar.tsx
+++ src/components/DisplayCharacters/CharacterAvatar.tsx
@@ -1,23 +1,30 @@
 import Image from "next/image";
 import React from "react";
 
+const DEFAULT_AVATAR_BASE64 =
+  "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkJCQkKCQoMDAoPEA4QDxUUEhIUFSAXGRcZFyAxHyQfHyQfMSw1KygrNSxOPTc3PU5aTEhMWm5iYm6Kg4q0tPIBCQkJCQoJCgwMCg8QDhAPFRQSEhQVIBcZFxkXIDEfJB8fJB8xLDUrKCs1LE49Nzc9TlpMSExabmJiboqDirS08v/CABEIAAoACgMBIgACEQEDEQH/xAApAAEBAQAAAAAAAAAAAAAAAAAEBQYBAQEAAAAAAAAAAAAAAAAAAAIE/9oADAMBAAIQAxAAAADeVTqnX//EACAQAAICAgAHAAAAAAAAAAAAAAEDAhEAEgQTMTNBcXL/2gAIAQEAAT8Aa1LATKOsaujHFA8pddNRigJI4fYXcvPrFdpfyM//xAAZEQABBQAAAAAAAAAAAAAAAAAAAQIDESL/2gAIAQIBAT8An0jLP//EABgRAQADAQAAAAAAAAAAAAAAAAEAAxFR/9oACAEDAQE/AKwBzs//2Q==";
+
 const CharacterAvatar = ({
   src,
   size,
   alt,
+  n,
 }: {
   size: number;
   src: string;
   alt: string;
+  n: number;
 }) => {
   return (
     <Image
-      priority
       className="rounded-full"
       src={src}
       width={size}
       height={size}
       alt={alt}
+      priority
+      placeholder="blur"
+      blurDataURL={DEFAULT_AVATAR_BASE64}
     />
   );
 };
